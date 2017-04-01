0ffc473b0e79eb342f49a987475a34f471d36afd
Chris Wood <woodcr5@gmail.com>
2017-04-01 21:10:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var evens = [0, 2, 4, 6, 8, 10]
 
-evens.forEach(even => { console.log(`${even} is not odd!`)})
 
-evens.forEaach((even, index, array) => {
-  debugger
-  console.log(`${even} is not odd!`)
-})
-
-function square(n) {
-  console.log(n*n)
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
 }
+
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+
+var animals = ['dog', 'fish', 'cat']
+
+doToElementsInArray(animals, changeCompletely)
+console.log(animals)
